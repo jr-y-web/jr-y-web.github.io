76f6aa2ab340249f3f80ba836f9856685129c2a5
alterEgo <ex_jinrong.ye@tcl.com>
2023-11-08 11:28:06 +0800

    style:更改博客issue标签

--- docs/.vitepress/theme/layout.vue
+++ docs/.vitepress/theme/layout.vue
@@ -35,7 +35,7 @@ onMounted(() => {
                             clientID: "1fc22a84d635f390d433", // 自己的clientID
                             clientSecret: "78084f7fb43f5d7136177556246e5784040bde13", // 自己的clientSecret
                             admin: ["jr-y-web"], // GitHub repository 所有者
-                            labels: ["comment"], // GitHub issue 的标签
+                            labels: ["Gitalk"], // GitHub issue 的标签
                             createIssueManually: false, //如果当前页面没有相应的 isssue 且登录的用户属于 admin，则会自动创建 issue。如果设置为 true，则显示一个初始化页面，创建 issue 需要点击 init 按钮。
                         });
                         gitment.render("gitalk-page-container");
